25d15bf8c4496635e151878992ccb415a4d9f98e
CharlieXiao <32426723+CharlieXiao@users.noreply.github.com>
2019-07-26 12:31:56 +0800

    添加两个页面，尚未实现具体功能
    同时添加整体小程序框架思维导图

--- pages/courseDetail/courseDetail.js
+++ pages/courseDetail/courseDetail.js
@@ -1,20 +1,18 @@
-// pages/myCourse/myCourse.js
+// pages/courseDetail/courseDetail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    hasCourse:false,
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.setNavigationBarTitle({
-      title: '我的课程',
-    })
+
   },
 
   /**
