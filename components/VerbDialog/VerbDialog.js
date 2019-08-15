391d6494f6ef198ed35e18b3e5b0c74c02f25ec0
CharlieXiao <32426723+CharlieXiao@users.noreply.github.com>
2019-08-15 16:49:58 +0800

    一些细节修改
    考虑注册微信小程序，加入查询单词接口，以便进一步开发

--- components/VerbDialog/VerbDialog.js
+++ components/VerbDialog/VerbDialog.js
@@ -3,6 +3,8 @@
 let usSpeech = undefined;
 let ukSpeech = undefined;
 
+let verb_pattern = /\w{1,4}\./g 
+
 Component({
   /**
    * 组件的属性列表
@@ -23,7 +25,7 @@ Component({
       us_phonetic: 'gʊd',
       uk_speech: 'http://fjdx.sc.chinaz.net/files/download/sound1/201310/3610.wav',
       us_speech: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46',
-      explains: ["adj. 好的；优良的；愉快的；虔诚的", "n. 好处；善行；慷慨的行为"],
+      explains: ["n. 日落；傍晚；晚霞；（法律的）自动废止期，效力消减期", "adj. 霞红色的；浅橘红色的；衰落的；最后期的；定期废止的"],
     },
   },
 
