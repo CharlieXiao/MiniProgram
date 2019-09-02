bc0592b47e94b74fa687cc2864565874b8c5c495
CharlieXiao <32426723+CharlieXiao@users.noreply.github.com>
2019-09-02 23:01:01 +0800

    一些细节修改
    完成用户生词本功能的设计与实现

--- pages/verbList/verbList.js
+++ pages/verbList/verbList.js
@@ -1,7 +1,7 @@
 // pages/verbList/verbList.js
 
 const app = getApp();
-const requset_url = app.globalData.requset_url;
+const request_url = app.globalData.request_url;
 
 Page({
 
@@ -12,10 +12,11 @@ Page({
 	data: {
 	  hasVerb:true,
     verbList: [
-      {verb: 'and',phonetic: '[ənd, ən, ænd]',explain: { pos: 'conj.', explain: '和，与；就；而且；但是；然后' },speech: 'xxxxxx'},
-      {verb: 'or',phonetic: '[ɔr]',explain: { pos: 'conj.', explain: '或，或者；还是；要不然' },speech: 'xxxxxx'},
-      {verb: 'not',phonetic: '[nɑt]',explain: { pos: 'adv.', explain: '表示否定，不' },speech: 'xxxxxx'},
+      {verb: 'and', id:'1' ,phonetic: '[ənd, ən, ænd]',trans: { pos: 'conj.', explain: '和，与' },speech: 'xxxxxx',notRemove:true},
+      {verb: 'or', id:'2' ,phonetic: '[ɔr]',trans: { pos: 'conj.', explain: '或，或者' },speech: 'xxxxxx',notRemove:true},
+      {verb: 'not', id:'3' ,phonetic: '[nɑt]',trans: { pos: 'adv.', explain: '表示否定，不' },speech: 'xxxxxx',notRemove:true},
     ],
+    removeVerbList:[]
 	},
 
 	/**
@@ -24,55 +25,94 @@ Page({
 	onLoad: function (options) {
     wx.setNavigationBarTitle({
       title: '生词笔记',
+    });
+
+    let that = this;
+
+    wx.request({
+      url: request_url+'/VerbList',
+      data:{
+        open_id:app.globalData.open_id,
+      },
+      method:'GET',
+      success: function(res){
+        console.log(res);
+        that.setData({
+          hasVerb:res.data.hasVerb,
+          verbList:res.data.verbList,
+        })
+      }
     })
 	},
 
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
+  onUnload: function(){
+    // 在页面退出时与服务器同步，删除所选单词，避免多次连接同步，尽量只同步一次
+    console.log(this.data.removeVerbList)
 
-	},
+    wx.request({
+      url: request_url+'/removeVerbList',
+      data:{
+        removeList:this.data.removeVerbList,
+        open_id:app.globalData.open_id,
+      },
+      method:'GET',
+      success: function(res){
+        console.log(res)
+      }
+    })
 
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
+  },
 
-	},
+  verbPron: function(event){
+    let src = event.currentTarget.dataset.src
+    const innerAudioContext = wx.createInnerAudioContext()
+    innerAudioContext.src = src
+    innerAudioContext.autoplay = true
 
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
+    console.log('开始播放')
+    
+    innerAudioContext.onEnded(function(){
+      // 播放完成后立即删除
+      innerAudioContext.destroy();
+      console.log('播放结束')
+    })
+  },
 
-	},
+  removeVerb: function(event){
+    console.log('移除单词')
 
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
+    let verbid = event.currentTarget.dataset.verbid;
+    let verbindex = event.currentTarget.dataset.verbindex;
+    let new_verbList = this.data.verbList;
+    let new_remove_verbList = this.data.removeVerbList;
 
-	},
+    new_verbList[verbindex].notRemove = false;
 
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
+    // 更新删除的单词列表
 
-	},
+    new_remove_verbList.push(verbid);
 
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
+    // 判断单词列表是否全都被删除了
 
-	},
+    let flag = true
 
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
+    for(let i=0;i<new_verbList.length;i++){
+      if(new_verbList[i].notRemove == true){
+        flag = false;
+        break;
+      }
+    }
+    
+    if(flag){
+      this.setData({
+        hasVerb:false
+      })
+    }
 
-	}
+    this.setData({
+      verbList:new_verbList,
+      removeVerbList:new_remove_verbList
+    })
+  }
+
 })
